494ac7131c93c136e4d397d28c8b2c005aa45ddb
Aqueuse <jaimaille@gmail.com>
2022-02-19 14:00:01 +0100

    add a config.json file for token and channel name

--- chat.py
+++ chat.py
@@ -1,23 +1,22 @@
 from twitchio.ext import commands
-import pprint
+import json
+
 
 class Bot(commands.Bot):
-
     def __init__(self):
+        json_file = open('default_config.json', 'r')
+        data = json.load(json_file)
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        super().__init__(token='token', prefix='?', initial_channels=['channelname'])
+        super().__init__(token=data["token"], prefix='?', initial_channels=[data["channel"]])
 
     async def event_ready(self):
         # We are logged in and ready to chat and use commands...
-        print(f'Connected on {self.nick} with success ! - Twitch Bot')
-
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
-        # Send a hello back!
-        await ctx.send(f'Hello {ctx.author.name}!')
+        print(f'Hello {self.nick}')
 
     async def event_message(self, ctx: commands.Context):
-        print(f"From {ctx.author.name}: {ctx.content}")
+        print(f"{ctx.author.name} : {ctx.content}")
+
+
 
 bot = Bot()
 bot.run()
